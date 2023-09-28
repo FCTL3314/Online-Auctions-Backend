71dbf1c8691e3158909f06840e6df7fe2eda992d
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-09-28 13:55:20 +0300

    Add and use pydantic_settings library.

--- app/config.py
+++ app/config.py
@@ -1,15 +1,28 @@
-import os
-
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
 
-class Config:
-    DEBUG = bool(os.environ.get('DEBUG'))
+class Config(BaseSettings):
+    DEBUG: bool = False
 
-    DATABASE_NAME = os.environ.get('DATABASE_NAME')
-    DATABASE_HOST = os.environ.get('DATABASE_HOST')
-    DATABASE_PORT = os.environ.get('DATABASE_PORT')
-    DATABASE_USER = os.environ.get('DATABASE_USER')
-    DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+    DATABASE_DRIVER: str = "postgresql+asyncpg"
+    DATABASE_NAME: str
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"{self.DATABASE_DRIVER}://"
+            f"{self.DATABASE_USER}:"
+            f"{self.DATABASE_PASSWORD}@"
+            f"{self.DATABASE_HOST}/"
+            f"{self.DATABASE_NAME}"
+        )
+
+
+config = Config()
